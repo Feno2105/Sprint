bebb4aa6f4bec827b493bc40d38ec9b1647b98c8
Feno2105 <mamyrandria2105@gmail.com>
2025-11-07 14:52:18 +0300

    restrancription de frontservlet

--- src/main/java/com/itu/methode/FrontController.java
+++ src/main/java/com/itu/methode/FrontController.java
@@ -15,30 +15,21 @@ import java.util.Map;
 @WebServlet("/*")
 public class FrontController extends HttpServlet {
     RequestDispatcher defaultDispatcher;
-    private Map<String,Object> Route = new HashMap<>();
 
     @Override
     public void init() {
         defaultDispatcher = getServletContext().getNamedDispatcher("default");
-        Route =  Scanne.trouverControllers_urls();
     }
     
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
         String path = req.getPathInfo();
-       Map<String,String> result =  Scanne.isOurs(path);
-        if (!result.isEmpty()) {
-            defaultServe(req, resp, result);
-        } else {
-             if (path == null) path = "/";
-            resp.setContentType("text/html;charset=UTF-8");
-            resp.getWriter().println("<h1> :: Tu as demandé : " + path + "</h1>");
-        }
+        if (path == null) path = "/";
+        resp.setContentType("text/html;charset=UTF-8");
+        resp.getWriter().println("<h1> :: Tu as demandé : " + path + "</h1>");
     }
-    private void defaultServe(HttpServletRequest req, HttpServletResponse res,Map<String,String> result) throws ServletException, IOException {
+    private void defaultServe(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         res.setContentType("text/html;charset=UTF-8");
-            res.getWriter().println("<h1>BIS :: Controller : " + result.get("controller") + "</h1>");
-            res.getWriter().println("<h1>BIS :: methode : " + result.get("methode") + "</h1>");
 
     }
 }
